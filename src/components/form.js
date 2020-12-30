1848269dfc2a6fc9c80dd41f4edefcba6837fa15
Harsh Verma <harshverma0362@gmail.com>
2020-12-30 20:23:54 +0530

    learnt and implimented useState hooks method to handle mouse enents and set css styling with that conditions

--- src/components/form.js
+++ src/components/form.js
@@ -1,13 +1,27 @@
-import react from 'react';
+import react, { useState } from 'react';
 import Input from './input';
 import Btn from './login_btn';
 
 const isRegistered = true;
 
 const Form = () => {
+
+    const [heading, setHeadingText] = useState('Hello');
+    const [isMouseOver, setMouseOver] = useState(false);
+
+    function handleMouseOver() {
+        setHeadingText(`${heading}, mouse over button`)
+        setMouseOver(true)
+    }
+
+    function handleMouseOut() {
+        setMouseOver(false);
+    }
+
     return (
         <div className="row justify-content-center align-items-center main-form">
             <div className="col-lg-3 col-md-6 col-10">
+                <h2 className="text-danger">{heading}</h2>
                 <form>
                     <div className="form-group d-flex flex-column">
                         <Input placeholder="Username" type="text" id="username" />
@@ -17,6 +31,7 @@ const Form = () => {
                         {isRegistered ? null : <Input placeholder="Confirm Password" type="password" id="cpassword" />}
                     </div>
                     {isRegistered ? <Btn text="Login" id="login-btn" /> : <Btn text="Register" id="register-btn" />}
+                    <button className="btn w-100 mt-3" style={isMouseOver ? { backgroundColor: 'black', color: 'white' } : { backgroundColor: 'white', color: 'black' }} onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>Submit</button>
                 </form>
             </div>
         </div>
